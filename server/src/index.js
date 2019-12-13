3cf4a7142c668dfd1b251639fe748105ec5bf49c
gz <l2241883@gmail.com>
2019-12-13 12:05:27 +0800

    first project commit

---
+++ server/src/index.js
@@ -0,0 +1,4 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({ typeDefs });
---
+++ server/src/schema.js
@@ -0,0 +1,7 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  # Your schema will go here
+`;
+
+module.exports = typeDefs;
